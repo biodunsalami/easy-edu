3d7931583bf9d607131e242cfeec04e1930f7c8a
Abiodun Salami <abiodunslam@gmail.com>
2022-01-28 00:30:50 +0100

    Made few changes to Course Fragment and refactored Download Fragment to Settings Fragment

--- app/src/main/java/ps/room/easyedu/fragments/HomeFragment.kt
+++ app/src/main/java/ps/room/easyedu/fragments/HomeFragment.kt
@@ -23,8 +23,22 @@ class HomeFragment : BaseFragment() {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
+        swapViews(isCourseAdded)
 
         return binding.root
+    }
+
+    //Swaps the views on the home fragment once user starts a course
+    private fun swapViews(boolean: Boolean){
+        if (boolean){
+            binding.homeConstraintLayout.visibility = View.GONE
+            binding.browseTextView.visibility = View.VISIBLE
+            binding.coursesRecyclerView.visibility = View.VISIBLE
+        }else{
+            binding.homeConstraintLayout.visibility = View.VISIBLE
+            binding.browseTextView.visibility = View.GONE
+            binding.coursesRecyclerView.visibility = View.GONE
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/ps/room/easyedu/fragments/SettingsFragment.kt
+++ app/src/main/java/ps/room/easyedu/fragments/SettingsFragment.kt
@@ -10,19 +10,19 @@ import androidx.navigation.NavGraph
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import ps.room.easyedu.R
-import ps.room.easyedu.databinding.FragmentDownloadsBinding
+import ps.room.easyedu.databinding.FragmentSettingsBinding
 
 
-class DownloadsFragment : BaseFragment() {
+class SettingsFragment : BaseFragment() {
 
-    private lateinit var binding: FragmentDownloadsBinding
+    private lateinit var binding: FragmentSettingsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentDownloadsBinding.inflate(inflater, container, false)
+        binding = FragmentSettingsBinding.inflate(inflater, container, false)
 
 
         return binding.root
@@ -31,7 +31,7 @@ class DownloadsFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.downloadCourseButton.setOnClickListener {
-            findNavController().navigate(R.id.action_downloadsFragment_to_browseFragment)
+            findNavController().navigate(R.id.action_settingsFragment_to_browseFragment)
         }
     }
 
